da5055d1346b4cc7317e56a5ca811efe84cb0722
DaMing <c3009023@newcastle.ac.uk>
2023-12-02 16:33:01 +0000

    net work

--- CSC8503CoreClasses/GameClient.cpp
+++ CSC8503CoreClasses/GameClient.cpp
@@ -1,22 +1,33 @@
 #include "GameClient.h"
 #include "./enet/enet.h"
+
 using namespace NCL;
 using namespace CSC8503;
 
-GameClient::GameClient()	{
-	netHandle = enet_host_create(nullptr, 1, 1, 0, 0);
+GameClient::GameClient() {
+    netHandle = enet_host_create(nullptr, 1, 1, 0, 0);
 }
 
-GameClient::~GameClient()	{
-	enet_host_destroy(netHandle);
+GameClient::~GameClient() {
+    enet_host_destroy(netHandle);
 }
 
 bool GameClient::Connect(uint8_t a, uint8_t b, uint8_t c, uint8_t d, int portNum) {
-	return false;
+    ENetAddress address;
+    address.port = portNum;
+    address.host = (d << 24) | (c << 16) | (b << 8) | (a);
+
+    netPeer = enet_host_connect(netHandle, &address, 2, 0);
+
+    return netPeer != nullptr;
+
 }
 
 void GameClient::UpdateClient() {
 }
 
-void GameClient::SendPacket(GamePacket&  payload) {
+void GameClient::SendPacket(GamePacket &payload) {
+    ENetPacket *dataPacket = enet_packet_create(&payload,payload.GetTotalSize(), 0);
+    enet_peer_send(netPeer, 0, dataPacket);
+
 }
